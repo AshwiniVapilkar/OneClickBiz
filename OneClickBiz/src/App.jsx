7e8e0fd46e19f81ffb14cfd9d56b95ceb57719ba
Ashwini Vapilkar <vapilkarashwini3@gmail.com>
2025-06-02 17:02:19 +0530

    give basic codewrite

--- OneClickBiz/src/App.jsx
+++ OneClickBiz/src/App.jsx
@@ -1,27 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-center text-gray-500">
-        Click on the Vite and React logos to learn more.
-      </p>
+      <h1 className='text-center font-semibold text-3xl'>New Feature Added</h1>
     </>
   )
 }
