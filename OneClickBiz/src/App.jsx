f24bc074d0885867c8f2f22eae2d6c72634049ef
Ashwini Vapilkar <vapilkarashwini3@gmail.com>
2025-06-02 16:50:30 +0530

    add new feature

--- OneClickBiz/src/App.jsx
+++ OneClickBiz/src/App.jsx
@@ -8,20 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-center text-gray-500">
-        Click on the Vite and React logos to learn more.
-      </p>
+     <h1>New Features</h1>
     </>
   )
 }
